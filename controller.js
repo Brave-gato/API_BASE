ca42c943aa62220dc6ae6545c7af43ca3f303a52
Brave-gato <webrepoleved@google.com>
2025-01-09 20:37:38 +0100

    Suppression de node_modules du dépôt

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 exports.hellow = (req, res) => {
-    db.db.collection("test1").findOne({ field:"Hello Lama World" }).then(result => {
+    db.db.collection("test1").findOne({ field:"Hello Lama World test" }).then(result => {
         res.status(201).json(result)
     }).catch(error => {
         console.log(error)
